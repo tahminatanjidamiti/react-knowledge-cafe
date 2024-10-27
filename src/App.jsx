db13cfef56689757585addc42da4e784285326ab
tahminatanjidamiti <mity2027@gmail.com>
2024-10-27 21:29:43 +0600

    added state, function and new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
 import Header from './Components/Header/Header'
 
 function App() {
+  const [bookmarks, setBookmarks] = useState([]);
+  const [readingTime, setReadingTime] = useState(0);
 
+  const handleAddToBookmark = blog =>{
+    const newBookmarks = [...bookmarks, blog];
+    setBookmarks(newBookmarks);
+  }
+  const handleMarkAsRead = (id, time) => {
+    const newReadingTime = (readingTime + time);
+    setReadingTime(newReadingTime);
+    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id !== id);
+    setBookmarks(remainingBookmarks);
+  }
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className='md:flex max-w-7xl mx-auto'>
+      <Blogs handleAddToBookmark={handleAddToBookmark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+      <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
+      </div>
     </>
   )
 }
